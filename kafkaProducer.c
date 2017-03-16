7d185beba1e7f938b4affc0133641c6943649988
Aneesh Puttur <aputtur@redhat.com>
2017-03-16 11:41:03 -0400

    Fixed library path

--- kafkaProducer.c
+++ kafkaProducer.c
@@ -1,15 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <syscall.h>
 #include <pthread.h>
 #include <librdkafka/rdkafka.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <getopt.h>
 #define MAX_SIZE 50
 #define NUM_CLIENT 5
 
 void *producer_launcher(void *thread_id);
+
+void sigint(int signo) {
+    (void)signo;
+}
+
+/**
+ *  * Kafka logger callback (optional)
+ *   */
+static void logger (const rd_kafka_t *rk, int level,
+		    const char *fac, const char *buf) {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	fprintf(stdout, "%u.%03u RDKAFKA-%i-%s: %s: %s\n",
+		(int)tv.tv_sec, (int)(tv.tv_usec / 1000),
+		level, fac, rd_kafka_name(rk), buf);
+}
+
 int main()
 {
 
@@ -37,34 +58,35 @@ int main()
      sigaction(SIGINT, &s, NULL);
     
     // Restore the old signal mask only for this thread.
-     pthread_sigmask(SIG_SETMASK, &oldset, NULL);
+    /* pthread_sigmask(SIG_SETMASK, &oldset, NULL);
     // Wait for SIGINT to arrive.
      pause();
     for (int i=1;i<=NUM_CLIENT;i++){
      pthread_cancel(client_thread[i]);
     }
      for (int i=1;i<=NUM_CLIENT;i++){
-     pthread_joinl(client_thread[i]);
-    }
+     pthread_join(client_thread[i],NULL);
+    }*/
 
     pthread_exit(NULL);
 
     return 0;
-}
-void sigint(int signo) {
-    (void)signo;
 }
 
 
 void *producer_launcher(void *threadid)
 {
     long threadnum = (long)threadid;
-
-    char *brokers="10.11.108.11:9092,10.11.108.12:9092,10.11.108.13:9092"
-    char *topic="FAULT_EVENT"
-    int partition=3
+    rd_kafka_conf_t *conf;
+    rd_kafka_topic_conf_t *topic_conf;
+    static rd_kafka_t *rk;
+    rd_kafka_topic_t *rkt;
+    unsigned long counter=0;
+    char errstr[512];
+    char *brokers="10.11.108.11:9092,10.11.108.12:9092,10.11.108.13:9092";
+    char *topic="FAULT_EVENT";
+    int partition=3;
     long messageCounter=0;
-    printf("Connected successfully client:%d\n", threadnum);
     pthread_t tid = pthread_self();
     printf("POSIX thread id is %d\n", tid);
     int sid = syscall(SYS_gettid);
@@ -72,6 +94,7 @@ void *producer_launcher(void *threadid)
 
     // KAFKA CONF              
         conf = rd_kafka_conf_new(); 
+        rd_kafka_conf_set_log_cb(conf, logger);
         /* producer config */
        // rd_kafka_conf_set(conf, "queue.buffering.max.messages", "600000", NULL, 0);
        //rd_kafka_conf_set(conf, "message.send.max.retries", "3", NULL, 0);
@@ -85,11 +108,13 @@ void *producer_launcher(void *threadid)
                                                  
         /* create Kafka handle */
         if (!(rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr)))) {
+         printf("Failed to connect %s",errstr);
         fprintf(stderr, "%% Failed to create new producer: %s\n", errstr);
          exit(1);
         }
         /* Add brokers */
          if (rd_kafka_brokers_add(rk, brokers) == 0) {
+            printf("No valid broker found:");
 	    fprintf(stderr, "%% No valid brokers specified\n");
 	 exit(1);
          }
@@ -127,15 +152,13 @@ void *producer_launcher(void *threadid)
                    // run=0;
                    // continue;
                 }
-
-         printf("%% ***** (%lu) Sent %s to topic %s *****\n", counter, buf, topic);
-
-
-        
+        printf("Sent...");
+        printf("%% Sent %zd bytes to topic "
+				"%s partition %i\n",len, rd_kafka_topic_name(rkt), partition);
 
         printf("For thread : %d\n", threadnum);
         printf("My Process Id: %d\n",sid);
-        printf("Brokers are %s\n",brokers)
+        printf("Brokers are %s\n",brokers);
         /* Poll to handle delivery reports */
         //rd_kafka_poll(rk, 0);
         sleep(10);
@@ -175,37 +198,3 @@ static void err_cb (rd_kafka_t *rk, int err, const char *reason, void *opaque) {
     printf("ERROR!\n");
 }
 
-void SendMessage(char buf){
-   if (buf[len - 1] == '\n')
-        buf[--len] = '\0'; 
-
-      /* Send/Produce message. */
-
-        int prod_result=rd_kafka_produce(rkt, partition, RD_KAFKA_MSG_F_COPY,
-                        /* Payload and length */
-                        buf, len,
-                        /* Optional key and its length */
-                        NULL, 0,
-                        /* Message opaque, provided in
-                          * delivery report callback as
-                         * msg_opaque. */
-                        NULL);
-         
-          printf("Produce return code: %d\n", prod_result);
-
-                if (prod_result == -1) {
-                    printf("Error on rd_kafka_produce function call\n");
-
-                    /* Poll to handle delivery reports */
-                    rd_kafka_poll(rk, 0);
-                    run=0;
-                    continue;
-                }
-
-         printf("%% ***** (%lu) Sent %s to topic %s *****\n", counter, buf, topic);
-
-     
-
-
-
-}
